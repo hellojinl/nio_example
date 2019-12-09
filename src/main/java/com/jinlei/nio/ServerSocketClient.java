b8bdf7499fa345536c25f1e9ab508e34053ab07a
hello_jinl <hello_jinl@126.com>
2019-12-09 19:22:58 +0800

    commit

---
+++ src/main/java/com/jinlei/nio/ServerSocketClient.java
@@ -0,0 +1,53 @@
+package com.jinlei.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author jinl
+ * @since 2019-12-05
+ */
+public class ServerSocketClient {
+
+    public static void main(String[] args) {
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        SocketChannel socketChannel = null;
+        try {
+            socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+            socketChannel.connect(new InetSocketAddress("localhost", 8080));
+            if (socketChannel.finishConnect()) {
+                int i = 0;
+                while (true) {
+                    TimeUnit.SECONDS.sleep(1);
+                    String info = "I'm" + (i++) + "-th information from client";
+                    buffer.clear();
+                    buffer.put(info.getBytes());
+                    buffer.flip();
+                    while (buffer.hasRemaining()) {
+                        System.out.println(buffer);
+                        socketChannel.write(buffer);
+                    }
+                }
+            }
+
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            if (socketChannel != null) {
+                try {
+                    socketChannel.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+
+    }
+}
